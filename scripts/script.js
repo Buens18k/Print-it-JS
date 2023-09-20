bc71f3ffbacdf14f32af63d84d2e58f0ef155c07
Bruno Vang <buens@outlook.fr>
2023-09-20 22:53:22 +0200

    condition pour chaque arrow

--- scripts/script.js
+++ scripts/script.js
@@ -17,69 +17,49 @@ const slides = [
     tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
   },
 ];
-// Récupères chaques images et sont tagline
-// parcours chaque objet
-// slides.forEach((image, tagLine) => console.log(image, tagLine))
 
-// let premierSlide = slides[0]
-// console.log(premierSlide)
-
-// Récupère les flèches
-const arrow_left = document.querySelector(".arrow_left");
-console.log(arrow_left);
-
-const arrow_right = document.querySelector(".arrow_right");
-console.log(arrow_right);
-
-// Ecoute évènement lors du click sur les flèches
-arrow_left.addEventListener("click", (event) => {
-  console.log("fleche gauche");
-});
-
-arrow_right.addEventListener("click", (event) => {
-  console.log("fleche droite");
-});
-
-// Créer les dots pour chaque slide
-
-// je récup le selecteur .dots
-const dotsDiv = document.querySelector(".dots");
 const imageElement = document.querySelector(`#banner .banner-img`);
-console.log(imageElement);
+// console.log(imageElement);
 const tagLineElement = document.querySelector(`#banner p`);
-console.log(tagLineElement);
+// console.log(tagLineElement);
+
+const dotsDiv = document.querySelector(".dots");
 
 const maxSlides = slides.length;
 
 let currentSlide = 0;
-// je parcours chaque objet dans la liste slides
-// et pour chaque slide tu fait
-slides.forEach((slide) => {
-  // création d'une div
-  const dot = document.createElement("div");
-  // avec la class "".dot"
-  dot.classList.add(`dot`);
-  // il sera l'enfant de la div "dots" (qui est récupêrer dans cette variable)
-  dotsDiv.appendChild(dot);
-});
 
-// Pour être SUR que chaque dot contient son slide relatifs à l'index
-// je récupère all dot et stock dans la variable dots
+function updateSlide(index) {
+  currentSlide = index;
+  // Mets a jour l'image
+  const slide = slides[currentSlide];
+  // console.log(currentSlide);
+  imageElement.src = `./assets/images/slideshow/${slide.image}`;
+  console.log(`Vous avez cliqué sur le dot qui représente le ${slide.image}`);
+
+  // Mets a jour le tagline
+  tagLineElement.innerHTML = slide.tagLine;
+  // console.log(`Et voici son tag:  ${tagLineElement.innerHTML}`)
+}
+
+function createDotCaroussel(liste) {
+  liste.forEach((slide) => {
+    // création d'un button
+    const dot = document.createElement("button");
+    // avec la class "".dot"
+    dot.classList.add(`dot`);
+    // il sera l'enfant de la div "dots" (qui est récupêrer dans cette variable)
+    dotsDiv.appendChild(dot);
+  });
+}
+// j'appelle la fonction qui créer le dot en joignant le nom de la liste
+createDotCaroussel(slides);
+
 const dots = document.querySelectorAll(".dot");
-// j'parcours chaque objet dans ma liste dots
-// Avec en paramètre
-dots.forEach((dot, index) => {
-  console.log(`Le Dot ${index + 1}:`, dots[index]);
-  console.log(
-    `Le Dot ${index + 1} contient le slide ${index + 1}:`,
-    slides[index]
-  );
-});
 
-// Ont applique un style à chaque dot selectionner
-
-// j'applique le style au dot
-dots[0].classList.add(`dot_selected`);
+// j'applique le style au dot au chargement
+dots[currentSlide].classList.add(`dot_selected`);
+// console.log(`le slide au chargement est le ${currentSlide}`)
 
 dots.forEach((dot, index) => {
   dot.addEventListener("click", (event) => {
@@ -95,14 +75,41 @@ dots.forEach((dot, index) => {
   });
 });
 
-function updateSlide(index) {
-  currentSlide = index;
-  // Mets a jour l'image
-  const slide = slides[currentSlide];
-  console.log(currentSlide);
-  imageElement.src = `./assets/images/slideshow/${slide.image}`;
-  console.log(`./assets/images/slideshow/${slide.image}`);
+// Récupère les flèches
+const arrow_left = document.querySelector(".arrow_left");
+// console.log(arrow_left);
 
-  // Mets a jour le tagline
-  tagLineElement.innerHTML = slide.tagLine;
-}
+const arrow_right = document.querySelector(".arrow_right");
+// console.log(arrow_right);
+
+// Ecoute évènement lors du click sur les flèches
+arrow_left.addEventListener("click", (event) => {
+  console.log("fleche gauche");
+  if (currentSlide === 0) {
+    updateSlide(maxSlides -1)
+  } else {
+    updateSlide(currentSlide -1)
+  }
+  
+});
+
+arrow_right.addEventListener("click", (event) => {
+  console.log("fleche droite");
+  if (currentSlide === maxSlides - 1) {
+    updateSlide(0)
+  } else {
+    updateSlide(currentSlide +1)
+  }
+});
+
+// // Pour être SUR que chaque dot contient son slide relatifs à l'index
+// // je récupère all dot et stock dans la variable dots
+// // j'parcours chaque objet dans ma liste dots
+// // Avec en paramètre
+// dots.forEach((dot, index) => {
+//   console.log(`Le Dot ${index + 1}:`, dots[index]);
+//   console.log(
+//     `Le Dot ${index + 1} contient le slide ${index + 1}:`,
+//     slides[index]
+//   );
+// });
